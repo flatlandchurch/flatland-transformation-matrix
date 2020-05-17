a210cc5a13b727e102a3c647524313f7fd8f599a
Matt McElwee <mmcelwee@dmsi.com>
2020-05-17 02:35:41 -0500

    Added interactivity and disciplines

---
+++ build.js
@@ -0,0 +1,99 @@
+const ejs = require("ejs");
+const path = require("path");
+const { promisify } = require("util");
+const fs = require("fs");
+const prettier = require("prettier");
+
+const goals = require("./goals");
+const disciplines = require("./disciplines");
+const version = require("./package").version;
+
+const render = promisify(ejs.renderFile);
+const write = promisify(fs.writeFile);
+
+const Card = ({ name, description, discipline, category, principles }) =>
+  `<div class="card">
+    <h2>${name}</h2>
+    <p>${description}</p>
+    <div class="card__details">
+        <div class="principles">
+            <strong>Principles</strong>
+            ${principles.map((p) => `<p>${p}</p>`).join("\n")}
+        </div>
+        <div class="discipline">
+            <strong>Discipline</strong>
+            <p>
+              <a href="#/${category.toLowerCase()}/${name
+    .toLowerCase()
+    .replace(
+      /\s/g,
+      "-"
+    )}/discipline/${discipline.toLowerCase()}">${discipline}</a>
+            </p>
+        </div>
+    </div>
+</div>`;
+
+const Category = ({ idx, content, name }) =>
+  `<div class="category${
+    idx === 0 ? " active" : ""
+  }" data-category="${name.toLowerCase()}">
+    ${content}
+</div>`;
+
+const Header = (title, content) =>
+  `<header>
+    <a href="" id="prev"><span class="material-icons">chevron_left</span></a>
+    <h1>${title}</h1>
+    <a href="" id="next"><span class="material-icons">chevron_right</span></a>
+</header>\n${content} 
+`;
+
+const Modal = ({ name, verse, practices }) => `
+<script type="template/html" data-disicpline="${name
+  .toLowerCase()
+  .replace(/\s/g, "-")}">
+    <h2>${name}</h2>
+    <div class="verse">
+        <p>${verse.content}</p>
+        <strong>${verse.ref}</strong>
+    </div>
+    <strong>Practices</strong>
+    <ul>
+        ${Object.keys(practices)
+          .map((k) => `<li data-parent="${k}">${practices[k]}</li>`)
+          .join("\n")}
+    </ul>
+</script>
+`;
+
+(async () => {
+  const categories = goals.reduce((acc, goal) => {
+    if (!acc[goal.category]) {
+      acc[goal.category] = [];
+    }
+    acc[goal.category].push(Card(goal));
+    return acc;
+  }, {});
+
+  const pages = Object.keys(categories)
+    .map((key, idx) =>
+      Category({ idx, content: categories[key].join("\n"), name: key })
+    )
+    .join("\n");
+
+  const content = [
+    Header(Object.keys(categories)[0], pages),
+    ...disciplines.map(Modal),
+  ].join("\n");
+
+  const html = await render(path.join(__dirname, "template.ejs"), {
+    content,
+    year: new Date().getFullYear(),
+    version,
+  });
+  await write(
+    path.join(__dirname, "index.html"),
+    prettier.format(html, { parser: "html" })
+  );
+})();
---
+++ load.js
@@ -0,0 +1,78 @@
+const ORDER = ['character', 'home', 'relationships', 'industry', 'society', 'talents'];
+
+function replaceHash() {
+  const path = getHash();
+  if (!path) {
+    window.history.replaceState('', null, '#/character');
+  }
+}
+
+function getHash() {
+  return window.location.hash.replace('#', '');
+}
+
+function getSegments(hash) {
+  return hash.split('/').slice(1);
+}
+
+function titleate(name) {
+  return `${name[0].toUpperCase()}${name.slice(1)}`;
+}
+
+function setUpNav([category]) {
+  const currentIdx = ORDER.findIndex((p) => p === category);
+
+  if (category === 'character') {
+    document.getElementById('next').href = `#/home`;
+    document.getElementById('prev').className = 'deactivate';
+  } else if (category === 'talents') {
+    document.getElementById('prev').href = `#/society`;
+    document.getElementById('next').className = 'deactivate';
+  } else {
+    const prev = document.getElementById('prev');
+    const next = document.getElementById('next');
+
+    prev.href = `#/${ORDER[currentIdx - 1]}`;
+    prev.className = '';
+    next.href = `#/${ORDER[currentIdx + 1]}`;
+    next.className = '';
+  }
+  document.querySelector('header h1').innerText = titleate(ORDER[currentIdx]);
+}
+
+function wait(ms) {
+  return new Promise((resolve) => {
+    return setTimeout(resolve, ms);
+  });
+}
+
+async function route([category, goal, type, sub]) {
+  document.querySelector('[data-category].active').className = 'category';
+  document.querySelector(`[data-category="${category}"]`).className = 'category active';
+
+  if (type) {
+    document.getElementById('discipline-modal').style.display = 'block';
+    await wait(200);
+    document.querySelector('#discipline-modal .modal__container').className = 'modal__container active';
+  } else {
+    document.getElementById('discipline-modal').style.display = 'none';
+  }
+}
+
+function closeModal() {
+  const [category] = getSegments(getHash());
+  window.location.hash = `#/${category}`;
+}
+
+(() => {
+  replaceHash();
+  const segments = getSegments(getHash());
+  setUpNav(segments);
+  route(segments);
+})();
+
+window.addEventListener('hashchange', (e) => {
+  const segments = getSegments(getHash());
+  setUpNav(segments);
+  route(segments);
+});
